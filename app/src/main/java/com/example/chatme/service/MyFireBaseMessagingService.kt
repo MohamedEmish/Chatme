9b87a762a06370c49e9a94ce916d9beeb7803edc
Mohamed Emish <emish52020@gmail.com>
2019-06-15 13:25:26 +0200

    15 June

--- app/src/main/java/com/example/chatme/service/MyFireBaseMessagingService.kt
+++ app/src/main/java/com/example/chatme/service/MyFireBaseMessagingService.kt
@@ -1,14 +1,66 @@
 package com.example.chatme.service
 
-import android.util.Log
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.example.chatme.MainActivity
+import com.example.chatme.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFireBaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        if (remoteMessage.notification != null) {
-            Log.d("FCM", remoteMessage.data.toString())
+//        if (remoteMessage.notification != null) {
+//            Log.d("FCM", remoteMessage.data.toString())
+//        }
+
+        sendNotification(remoteMessage.notification!!.body)
+
+    }
+
+    private fun sendNotification(messageBody: String?) {
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val channelId = "channel-01"
+        val channelName = getString(R.string.app_name)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+
+            val mChannel = NotificationChannel(
+                channelId, channelName, importance
+            )
+            notificationManager.createNotificationChannel(mChannel)
         }
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val mBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_fire_emoji)
+            .setColor(ContextCompat.getColor(this, R.color.colorAccent))
+//            .setContentTitle(title)
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+
+        val stackBuilder = TaskStackBuilder.create(this)
+        stackBuilder.addNextIntent(intent)
+        val resultPendingIntent = stackBuilder.getPendingIntent(
+            1,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        mBuilder.setContentIntent(resultPendingIntent)
+
+        notificationManager.notify(1, mBuilder.build())
     }
 }
